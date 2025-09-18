f3fda599d002f173f68ef592a0f65de77766a2cd
mrkbingham <mark.bingham@interworks.com>
2025-09-18 12:03:45 -0500

    verifies modal content

--- tests/Feature/CustomerResourceTest.php
+++ tests/Feature/CustomerResourceTest.php
@@ -2,8 +2,10 @@
 
 use App\Filament\Resources\Customers\Pages\EditCustomer;
 use App\Models\Customer;
+use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\Testing\TestAction;
+use Illuminate\Support\HtmlString;
 
 use function Pest\Livewire\livewire;
 
@@ -18,7 +20,12 @@ it('cannot delete customer without confirmation - action class', function () {
         'record' => $customer->id,
     ])
         ->assertHasNoFormErrors()
-        ->assertActionExists(DeleteAction::class)
+        ->assertActionExists(DeleteAction::class, checkActionUsing: function (Action $action) {
+            // Ensure the modal is properly set up
+            expect($action->getModalContent()->toHtml())->toBe('Header modal content');
+
+            return true;
+        })
         ->mountAction(DeleteAction::class)
         // ->callAction(DeleteAction::class)
         ->assertActionMounted(DeleteAction::class)
@@ -36,7 +43,12 @@ it('cannot delete customer without confirmation - test actions', function () {
         'record' => $customer->id,
     ])
         ->assertHasNoFormErrors()
-        ->assertActionExists(TestAction::make(DeleteAction::class))
+        ->assertActionExists(TestAction::make(DeleteAction::class), checkActionUsing: function (Action $action) {
+            // Ensure the modal is properly set up
+            expect($action->getModalContent()->toHtml())->toBe('Header modal content');
+
+            return true;
+        })
         ->mountAction(TestAction::make(DeleteAction::class))
         // ->callAction(TestAction::make(DeleteAction::class))
         ->assertActionMounted(TestAction::make(DeleteAction::class))
@@ -54,7 +66,13 @@ it('cannot delete customer without confirmation - form delete action', function 
         'record' => $customer->id,
     ])
         ->assertHasNoFormErrors()
-        ->assertActionExists(TestAction::make('formDeleteAction'))
+        // This fails - but the action should be accessible?
+        ->assertActionExists(TestAction::make('formDeleteAction'), checkActionUsing: function (Action $action) {
+            // Ensure the modal is properly set up
+            expect($action->getModalContent()->toHtml())->toBe('Form action modal content');
+
+            return true;
+        })
         ->mountAction(TestAction::make('formDeleteAction'))
         // ->callAction(TestAction::make('formDeleteAction'))
         ->assertActionMounted(TestAction::make('formDeleteAction'))
@@ -72,7 +90,12 @@ it('cannot delete customer without confirmation - nested form delete action', fu
         'record' => $customer->id,
     ])
         ->assertHasNoFormErrors()
-        ->assertActionExists(TestAction::make('nestedFormDeleteAction')->schemaComponent('group'))
+        ->assertActionExists(TestAction::make('nestedFormDeleteAction')->schemaComponent('group'), checkActionUsing: function (Action $action) {
+            // Ensure the modal is properly set up
+            expect($action->getModalContent()->toHtml())->toBe('Nested form action modal content');
+
+            return true;
+        })
         ->mountAction(TestAction::make('nestedFormDeleteAction')->schemaComponent('group'))
         ->assertActionMounted(TestAction::make('nestedFormDeleteAction')->schemaComponent('group'))
         ->assertSee('Nested form action modal content');
